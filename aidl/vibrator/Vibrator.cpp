1864b576bd8a7ff2010bb34d8de828a41f96aa1f
SamarV-121 <samarvispute121@pm.me>
2023-03-30 20:39:37 +0530

    vibrator: Re-implement TEXTURE_TICK effect
     * also refactor a bit
    
    Change-Id: I72e9131e6847474c3f06bc37fa83707de77e2285

--- aidl/vibrator/Vibrator.cpp
+++ aidl/vibrator/Vibrator.cpp
@@ -20,6 +20,8 @@ namespace android {
 namespace hardware {
 namespace vibrator {
 
+static constexpr const char* kVibratorDurationProp = "ro.vendor.vibrator_hal.duration.";
+
 static std::map<Effect, int> CP_TRIGGER_EFFECTS {
     { Effect::CLICK, 10 },
     { Effect::DOUBLE_CLICK, 14 },
@@ -55,13 +57,15 @@ static bool nodeExists(const std::string& path) {
     return f.good();
 }
 
+template <typename T>
+static inline int getDurationProperty(std::string key, const T def) {
+    return ::android::base::GetIntProperty(kVibratorDurationProp + key, def);
+}
+
 Vibrator::Vibrator() {
     mIsTimedOutVibrator = nodeExists(VIBRATOR_TIMEOUT_PATH);
     mHasTimedOutIntensity = nodeExists(VIBRATOR_INTENSITY_PATH);
     mHasTimedOutEffect = nodeExists(VIBRATOR_CP_TRIGGER_PATH);
-
-    mClickDuration = ::android::base::GetIntProperty("ro.vendor.vibrator_hal.click_duration", mClickDuration);
-    mTickDuration = ::android::base::GetIntProperty("ro.vendor.vibrator_hal.tick_duration", mTickDuration);
 }
 
 ndk::ScopedAStatus Vibrator::getCapabilities(int32_t* _aidl_return) {
@@ -142,7 +146,7 @@ ndk::ScopedAStatus Vibrator::perform(Effect effect, EffectStrength strength, con
 }
 
 ndk::ScopedAStatus Vibrator::getSupportedEffects(std::vector<Effect>* _aidl_return) {
-    *_aidl_return = { Effect::CLICK, Effect::TICK };
+    *_aidl_return = { Effect::CLICK, Effect::TICK, Effect::TEXTURE_TICK };
 
     if (mHasTimedOutEffect) {
       for (const auto& effect : CP_TRIGGER_EFFECTS) {
@@ -281,9 +285,11 @@ uint32_t Vibrator::effectToMs(Effect effect, ndk::ScopedAStatus* status) {
     *status = ndk::ScopedAStatus::ok();
     switch (effect) {
         case Effect::CLICK:
-            return mClickDuration;
+            return getDurationProperty("click", 10);
         case Effect::TICK:
-            return mTickDuration;
+            return getDurationProperty("tick", 5);
+        case Effect::TEXTURE_TICK:
+            return getDurationProperty("texture_tick", 5);
         default:
             break;
     }
